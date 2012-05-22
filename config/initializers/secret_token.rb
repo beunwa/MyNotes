69c1b741612d6636e656b5d4bea4f31206622c07
Beunwa <info@divioseo.fr>
2012-05-22 16:44:39 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNotes::Application.config.secret_token = 'f4ff60b4faa22388a7a977744768bb45b185d3af1cb397d6920c2c624d7e5fff4e5bdeb926e7aed26fb769952f9df15592c6d6d0e86b7af06500bebe29a23c21'
